ea62e433a882bebabfc19d498aaab53b80997f4c
ColeHouston <colehouston44@gmail.com>
2020-03-29 01:41:52 +0000

    added directory for rfcat to install to

--- install_rfcat.sh
+++ install_rfcat.sh
@@ -1,6 +1,6 @@
 sudo apt install -y ipython python-pip python3-pip
-git clone https://github.com/atlas0fd00m/rfcat.git
-cd rfcat
+git clone https://github.com/atlas0fd00m/rfcat.git $HOME/rfcat
+cd $HOME/rfcat
 sudo python setup.py build
 sudo python setup.py install
 sudo pip install -e .
